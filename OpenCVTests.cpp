97e80bde1746ab3746e1cf81217a974524afdcf3
Unknown <geohwk@gmail.com>
2019-12-11 03:28:23 +0000

    Using webcam, giving digital control (Left or Right)

--- OpenCVTests.cpp
+++ OpenCVTests.cpp
@@ -4,7 +4,11 @@
 #include <string>
 #include <thread>
 #include <chrono>
-
+#include <time.h>
+#pragma warning(disable:4996)
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 
 using namespace cv;
 using namespace std;
@@ -12,7 +16,7 @@ using std::cout;
 
 
 //CONSTANTS
-const int contourRatio = 5; //contour aspect ratio less than this size gets disregarded
+const int contourRatio = 3; //contour aspect ratio less than this size gets disregarded
 
 //GLOBALS
 int Hu = 100, Su = 255, Vu = 255, Hl = 0, Sl = 150, Vl = 150;
@@ -22,16 +26,23 @@ Point2f rect_points2[4];
 Point pt1, pt2, centralPt1, centralPt2, centralPt3, centralPt4;
 int frameIncrement = 0;
 
-
-
+String Difference;
+String Direction;
 string imagePath;
-
+double differenceVal;
 double RectPoints1Array[4] = { 0 };
 double RectPoints2Array[4] = { 0 };
 int closestPointLeft = 0;
 int closestPointRight = 0;
 double difference1;
 double difference2;
+
+double storedArea1 = 0;
+double storedArea2 = 0;
+double area = 0;
+int contour1 = 0;
+int contour2 = 0;
+int rightLine = 0;
 
 void trackbars()
 {
@@ -47,8 +58,14 @@ void trackbars()
 
 int main(int argc, char* argv[])
 {
+	time_t my_time = time(NULL);
 	VideoCapture cap(0); // open the video camera no. 0
 
+	RNG rng(12345);
+
+	Mat drawing1, drawing2, mask1, mask2, background, hsv;
+	Scalar color = Scalar(rng.uniform(0, 0), rng.uniform(0, 255), rng.uniform(0, 255));
+	Scalar color2 = Scalar(rng.uniform(0, 255), rng.uniform(0, 0), rng.uniform(0, 255));
 	if (!cap.isOpened())  // if not success, exit program
 	{
 		cout << "Cannot open the video cam" << endl;
@@ -57,88 +74,49 @@ int main(int argc, char* argv[])
 	 
 	trackbars();
 
-	/*
-	for (int i = 0; i < 30; i++)
-	{
-		cap >> background;
-	}
-	*/
+	//Create contour object
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	
 
 	//Invert the image
 	//flip(background, background, 1);
 	
 	//frame = background;
 	while(1){
-
-		frameIncrement++;
-		switch (frameIncrement) {
-		case 1: imagePath = "FakeRoad1.png";
-			break;       
-		case 10: imagePath = "FakeRoad2.png";
-			break;
-		case 20: imagePath = "FakeRoad3.png";
-			break;
-		case 30: imagePath = "FakeRoad4.png";
-			break;
-		case 40: imagePath = "FakeRoad5.png";
-			break;
-		case 50: imagePath = "FakeRoad6.png";
-			break;
-		case 60:frameIncrement = 0;
-			break;
-		default:
-			break;
-		}
-
-
-
-		Mat background = imread(imagePath);
-		//tuning();
-		//Mat frame;
-		//Capture frame-by-frame
-		//cap >> frame;
-		
-		//Laterally invert the image / flip the image
-		//flip(frame, frame, 1);
-
-
-
+		//for (int i = 0; i < 30; i++)
+		//{
+			cap >> background;
+		//}
+		time_t my_time = time(NULL);
+		//printf("Start of HSV: %s", ctime(&my_time));
 		//Converting image from BGR to HSV color space.
-		Mat hsv;
 		cvtColor(background, hsv, COLOR_BGR2HSV);
 
-		Mat mask1, mask2;
 		// Creating masks to detect the upper and lower red color.
 		inRange(hsv, Scalar(Hl, Sl, Vl), Scalar(Hu, Su, Vu), mask1);
-		//inRange(hsv, Scalar(170, 120, 70), Scalar(180, 255, 255), mask2);
 
-		// Combine masks together
-		//mask1 = mask1 + mask2;
-
-		RNG rng(12345);
-
-		//Create contour object
-		vector<vector<Point>> contours;
-		vector<Vec4i> hierarchy;
-
+		
+		
+		//printf("Start of Find Contours: %s", ctime(&my_time));
 		//Find contours from binary image
 		findContours(mask1, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-		
+		//printf("End of Find Contours: %s", ctime(&my_time));
 		//Make drawing canvas 
-		Mat drawing1 = Mat::zeros(mask1.size(), CV_8UC3);
-		Mat drawing2 = Mat::zeros(mask1.size(), CV_8UC3);
+		drawing1 = Mat::zeros(mask1.size(), CV_8UC3);
+		drawing2 = Mat::zeros(mask1.size(), CV_8UC3);
 		
 		//Minimum Area Rectangle object creation
 		vector<RotatedRect> minRect1(contours.size());
 		vector<RotatedRect> minRect2(contours.size());
 
-		double storedArea1 = 0;
-		double storedArea2 = 0;
-		double area = 0;
-		int contour1 = 0;
-		int contour2 = 0;
-		int rightLine = 0;
+		storedArea1 = 0;
+		storedArea2 = 0;
+		area = 0;
+		contour1 = 0;
+		contour2 = 0;
 	
+		//printf("Start of sorting: %s", ctime(&my_time));
 		if (contours.size() > 0) //If array of contours is greater than 0
 		{
 			for (int i = 0; i < contours.size(); i++) //Inrement through contour array
@@ -161,15 +139,9 @@ int main(int argc, char* argv[])
 
 			minRect1[contour1] = minAreaRect(Mat(contours[contour1]));
 			minRect2[contour2] = minAreaRect(Mat(contours[contour2]));
-			Scalar color = Scalar(rng.uniform(0, 0), rng.uniform(0, 255), rng.uniform(0, 255));
-			Scalar color2 = Scalar(rng.uniform(0, 255), rng.uniform(0, 0), rng.uniform(0, 255));
-
 			
-
 			if (minRect1[contour1].center.x > minRect2[contour2].center.x)
 			{
-				//minRect1[contour1] = minAreaRect(Mat(contours[contour2]));
-				//minRect2[contour2] = minAreaRect(Mat(contours[contour1]));
 				minRect1[contour1].points(rect_points2);
 				minRect2[contour2].points(rect_points1);
 			}
@@ -177,10 +149,6 @@ int main(int argc, char* argv[])
 				minRect1[contour1].points(rect_points1);
 				minRect2[contour2].points(rect_points2);
 			}
-			
-
-
-
 			if ((minRect1[contour1].size.height/minRect1[contour1].size.width > contourRatio)|| (minRect1[contour1].size.width / minRect1[contour1].size.height > contourRatio))
 			{
 				for (int j = 0; j < 4; j++)line(drawing1, rect_points1[j], rect_points1[(j + 1) % 4], color, 5, 8);
@@ -208,7 +176,6 @@ int main(int argc, char* argv[])
 
 						centralPt2.x = rect_points1[closestPointRight].x;
 						centralPt2.y = background.rows;
-
 					}
 				}
 				else
@@ -260,55 +227,11 @@ int main(int argc, char* argv[])
 					}
 				}
 		}
-
-		//Central line
-
-		double centralLine = 0;
-
-		//centralLine = (RectPoints1Array[closestPoint1] - RectPoints2Array[closestPoint2])/2 + RectPoints1Array[closestPoint1];
-
-		double differenceVal;
-		
+		//printf("Start of End: %s", ctime(&my_time));
 		differenceVal = difference1 - difference2;
 		
+		//cout << "Difference From center:" << differenceVal << endl;
 		
-
-		cout << "Difference From center:" << differenceVal << endl;
-		//cout << "difference2:" << difference2 << endl;
-
-		
-		/*
-
-		if (rightLine == 1)
-		{
-			centralPt1.x = RectPoints1Array[closestPoint1];
-			centralPt1.y = 0;
-
-			centralPt2.x = RectPoints1Array[closestPoint1];
-			centralPt2.y = background.rows;
-
-			centralPt3.x = RectPoints2Array[closestPoint2];
-			centralPt3.y = 0;
-
-			centralPt4.x = RectPoints2Array[closestPoint2];
-			centralPt4.y = background.rows;
-		}
-		else {
-			centralPt1.x = RectPoints2Array[closestPoint2];
-			centralPt1.y = 0;
-
-			centralPt2.x = RectPoints2Array[closestPoint2];
-			centralPt2.y = background.rows;
-
-			centralPt3.x = RectPoints1Array[closestPoint1];
-			centralPt3.y = 0;
-
-			centralPt4.x = RectPoints1Array[closestPoint1];
-			centralPt4.y = background.rows;
-		}
-		*/
-
-
 		pt1.x = background.cols / 2;
 		pt1.y = 0;
 
@@ -321,14 +244,21 @@ int main(int argc, char* argv[])
 		line(background, centralPt1, centralPt2, CV_RGB(0, 0, 255));
 		line(background, centralPt3, centralPt4, CV_RGB(0, 255, 0));
 
-		String Difference = "Difference: " + to_string(differenceVal);
+		Difference = "Difference: " + to_string(differenceVal);
+		if(differenceVal < 0){
+			Direction = "Direction: Turn Left";
+		}
+		else {
+			Direction = "Direction: Turn Right";
+		}
 
-		putText(background, Difference, Point(30, 30),
-			FONT_HERSHEY_PLAIN, 0.8, Scalar(200, 200, 250), 1, false);
+		putText(background, Difference, Point(30, 30),FONT_HERSHEY_PLAIN, 0.8, Scalar(200, 200, 250), 1, false);
+		putText(background, Direction, Point(30, 50), FONT_HERSHEY_PLAIN, 0.8, Scalar(200, 200, 250), 1, false);
 		//Creat Viewing Windows
 		imshow("Original Image", background);
-		imshow("Binary", mask1);
+		//imshow("Binary", mask1);
 		waitKey(1);
+		//printf("Difference: %d/r", differenceVal);
 		
 	}
 	
